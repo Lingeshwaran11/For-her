d668beada74eb958f79eb735516498476a1a2deb
Lingeshwaran V <lingeshwaran11112020@gmail.com>
2026-06-25 08:10:41 +0530

    added stars background

--- script/index.js
+++ script/index.js
@@ -606,6 +606,16 @@ function blowCandles() {
     }
   }, 800);
 }
+for (let i = 0; i < 200; i++) {
+  const star = document.createElement("div");
+  star.classList.add("star");
+
+  star.style.left = Math.random() * 100 + "vw";
+  star.style.top = Math.random() * 100 + "vh";
+  star.style.animationDelay = Math.random() * 5 + "s";
+
+  document.body.appendChild(star);
+}
 
 // Balloons effect old
 /*function startBalloons() {
